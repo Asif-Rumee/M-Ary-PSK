d10c1f44781ade4303f59bfed2a5fcb8cf35eaba
Asif Rahman Rumee <52595325+Asif-Rumee@users.noreply.github.com>
2021-12-31 14:26:18 +0600

    Update mpsk_performance.py

--- mpsk_performance.py
+++ mpsk_performance.py
@@ -1,12 +1,11 @@
 """
-Performance analysis of M-ary PSK over AWGN channel by SER vs Es/N0 chart.
+Performance analysis of M-ary PSK over AWGN channel by BER vs Eb/N0 chart.
 Author: Asif Rahman Rumee
 Date: 23/12/2021
 
 """
 import numpy as np 
 import matplotlib.pyplot as plt 
-from scipy.special import erfc
 from mpsk_modem import mpsk_modem, awgn
 
 nSym = 10**4
@@ -15,23 +14,34 @@ arrayOfM = [2,4,8,16,32]
 colors = ['k', 'b', 'r', 'm', 'y']
 fig, ax = plt.subplots(nrows=1,ncols=1)
 
+def bit_differences(symbol_a, symbol_b):
+	XOR = symbol_a ^ symbol_b
+	count = 0
+	while (XOR):
+		XOR = XOR & (XOR - 1)
+		count += 1
+	return count
+
 for i, M in enumerate(arrayOfM):
     k=np.log2(M)
-    EsN0dBs = 10*np.log10(k)+EbN0dBs 
-    SER = np.zeros(len(EbN0dBs)) 
+    BER = np.zeros(len(EbN0dBs)) 
     inputSyms = np.random.randint(low=0, high = M, size=nSym)
     modem = mpsk_modem(M) 
     modulatedSyms = modem.modulate(inputSyms)
 
-    for j,EsN0dB in enumerate(EsN0dBs):
-        receivedSyms = awgn(modulatedSyms,EsN0dB)
+    for j,EbN0dB in enumerate(EbN0dBs):
+        receivedSyms = awgn(modulatedSyms,EbN0dB)
         detectedSyms = modem.demodulate(receivedSyms)
-        SER[j] = np.sum(detectedSyms != inputSyms)/nSym
+        total = 0
+        for k,ds in enumerate(detectedSyms):
+            if inputSyms[k] != ds:
+                total += bit_differences(inputSyms[k], ds)
+        BER[j] = total/nSym*k
 
-    ax.semilogy(EbN0dBs,SER,color=colors[i],marker='o',linestyle='-',label='M = '+str(M))
+    ax.semilogy(EbN0dBs,BER,color=colors[i],marker='o',linestyle='-',label='M = '+str(M))
     
-ax.set_xlabel('Es/N0(dB)')
-ax.set_ylabel('SER ($P_s$)')
-ax.set_title('Performance analysis of Symbol Error Rate for MPSK over AWGN')
+ax.set_xlabel('Eb/N0(dB)')
+ax.set_ylabel('BER ($P_s$)')
+ax.set_title('Performance analysis of MPSK over AWGN')
 ax.legend()
 plt.show()
