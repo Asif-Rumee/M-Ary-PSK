cfcde8d999736703cb23e0b73f87f37d32dd6f36
Asif Rahman Rumee <52595325+Asif-Rumee@users.noreply.github.com>
2022-01-05 09:44:02 +0600

    Update mpsk_performance.py

--- mpsk_performance.py
+++ mpsk_performance.py
@@ -12,20 +12,13 @@ nSym = 10**4
 EbN0dBs = np.arange(start=-2,stop = 26, step = 2)
 arrayOfM = [2,4,8,16,32]
 colors = ['k', 'b', 'r', 'm', 'y']
+signs = ['s', 'v', '^', '*', '.']
 fig, ax = plt.subplots(nrows=1,ncols=1)
 
-def bit_differences(symbol_a, symbol_b):
-	XOR = symbol_a ^ symbol_b
-	count = 0
-	while (XOR):
-		XOR = XOR & (XOR - 1)
-		count += 1
-	return count
-
 for i, M in enumerate(arrayOfM):
-    k=np.log2(M)
-    EsN0dBs = 10*np.log10(k)+EbN0dBs 
-    BER = np.zeros(len(EbN0dBs)) 
+    m=np.log2(M)
+    EsN0dBs = 10*np.log10(m)+EbN0dBs 
+    SER = np.zeros(len(EbN0dBs)) 
     inputSyms = np.random.randint(low=0, high = M, size=nSym)
     modem = mpsk_modem(M) 
     modulatedSyms = modem.modulate(inputSyms)
@@ -33,16 +26,12 @@ for i, M in enumerate(arrayOfM):
     for j,EsN0dB in enumerate(EsN0dBs):
         receivedSyms = awgn(modulatedSyms,EsN0dB)
         detectedSyms = modem.demodulate(receivedSyms)
-        total = 0
-        for k,ds in enumerate(detectedSyms):
-            if inputSyms[k] != ds:
-                total += bit_differences(inputSyms[k], ds)
-        BER[j] = total/nSym*k
+        SER[j] = np.sum(detectedSyms != inputSyms)/nSym
 
-    ax.semilogy(EbN0dBs,BER,color=colors[i],marker='o',linestyle='-',label='M = '+str(M))
+    ax.semilogy(EbN0dBs,SER,color=colors[i],marker=signs[i],linestyle='-',label='M = '+str(M))
     
 ax.set_xlabel('Eb/N0(dB)')
-ax.set_ylabel('BER ($P_s$)')
-ax.set_title('Performance analysis of MPSK over AWGN')
+ax.set_ylabel('SER')
+ax.set_title('Performance analysis of M-PSK over AWGN')
 ax.legend()
 plt.show()
